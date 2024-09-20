c5b5048cb0857aa52fcd7638f8db94e332a44816
CppBerserker <157999806+CppBerserker@users.noreply.github.com>
2024-09-20 11:38:17 +0200

    Add connexion à la BDD

---
+++ Site/includes/connexionBDD.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$message = "";
+$message_class = "";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ftat"; //database name
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    $message = "Échec de la connexion :( " . $e->getMessage();
+    $message_class = "error";
+}
+?>
