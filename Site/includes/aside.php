b07648d7cfd2459a7758d2cec0673761e38ae765
Eudes <eudes.charlot@etu.uca.fr>
2024-09-21 16:13:23 +0200

    modification de la BDD ajout de aside.php

---
+++ Site/includes/aside.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<aside>
+  <ul>
+    <li>
+      <a href="#">Lien 1 : Ressources utiles</a>
+    </li>
+    <li>
+      <a href="#">Lien 2 : Tutoriels</a>
+    </li>
+    <li>
+      <a href="#">Lien 3 : Documentation</a>
+    </li>
+  </ul>
+</aside>
+
+</body>
+</html>
