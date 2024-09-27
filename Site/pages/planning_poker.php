745305fc375622c070c464ac5667fd3c476d4c09
Malgrave <rodeurwill@gmail.com>
2024-09-27 15:56:32 +0200

    Fix end

--- Site/pages/planning_poker.php
+++ Site/pages/planning_poker.php
@@ -5,83 +5,82 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - LOMAN's</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.min.css">
 </head>
 
 <body>
     <?php
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+    session_start();
+
     $_SESSION["currPrj"] = 1;
     $_SESSION["IdUser"] = 1;
-    include_once "../process/function_tasks.php";
-    $itemsPerPage = 1;
-    $results = displayAllTasks($_SESSION["currPrj"]);
-    $totalPages = ceil(count($results) / $itemsPerPage);
-    $currentPage = isset($_GET['page']) ? intval($_GET['page']) : 1;
 
-    if ($currentPage < 1 || $currentPage > $totalPages) {
-        $currentPage = 1;
-        header("Location: ?page=$currentPage");
+    if (!isset($_SESSION['NextPage'])) {
+        $_SESSION['NextPage'] = 0;
     }
-
+    if (!isset($_SESSION['SessionPage'])) {
+        $_SESSION['SessionPage'] = 0;
+    }
+    include_once "../process/function_tasks.php";
+    $results = displayAllTasks($_SESSION["currPrj"]);
     $difficulty = "?";
-    $startIndex = ($currentPage - 1) * $itemsPerPage;
-    $limitedResults = array_slice($results, $startIndex, $itemsPerPage);
 
-    if ($limitedResults && count($limitedResults) > 0) {
-        $currentTask = end($limitedResults);
-        unset($limitedResults[array_key_last($limitedResults)]);
-        $limitedResults[] = $currentTask;
+    if ($_SESSION["NextPage"] >= count($results)):
+
+        header("Location:../index.php");
+
+
+    else:
     ?>
         <table border="1">
             <thead>
                 <tr>
+                    <th>ID</th>
                     <th>Titre</th>
                     <th>User Story</th>
                     <th>Difficulté</th>
                 </tr>
             </thead>
             <tbody id="task-body">
-                <?php foreach ($limitedResults as $row): ?>
-                    <tr class="task-row" data-task-id="<?php echo htmlspecialchars($row['IdT']); ?>">
-                        <td><?php echo htmlspecialchars($row['TitreT']); ?></td>
-                        <td><?php echo htmlspecialchars($row['UserStoryT']); ?></td>
-                        <td><span id="difficulty-display"><?php echo htmlspecialchars($difficulty); ?></span></td>
+                <?php if ($results && count($results) > 0):
+                    $taskId = $results[$_SESSION["NextPage"]]['IdT'];
+                    $taskTitle = htmlspecialchars($results[$_SESSION["NextPage"]]['TitreT']);
+                    $taskStory = htmlspecialchars($results[$_SESSION["NextPage"]]['UserStoryT']);
+                    $difficultyDisplay = htmlspecialchars($difficulty);
+                ?>
+                    <tr class="task-row" data-task-id="<?php echo $taskId; ?>">
+                        <td><?php echo $taskId; ?></td>
+                        <td><?php echo $taskTitle; ?></td>
+                        <td><?php echo $taskStory; ?></td>
+                        <td><span id="difficulty-display"><?php echo $difficultyDisplay; ?></span></td>
                     </tr>
-                <?php endforeach; ?>
+                <?php endif; ?>
             </tbody>
         </table>
-    <?php
-    } else {
-        echo "Aucune tâche trouvée.";
-    }
-    ?>
 
-    <br>
-    <div class="button-container">
-        <button onclick="setDifficulty(this)" data-difficulty="?" class="button-choix">?</button>
-        <button onclick="setDifficulty(this)" data-difficulty="1" class="button-choix">1</button>
-        <button onclick="setDifficulty(this)" data-difficulty="3" class="button-choix">3</button>
-        <button onclick="setDifficulty(this)" data-difficulty="5" class="button-choix">5</button>
-        <button onclick="setDifficulty(this)" data-difficulty="10" class="button-choix">10</button>
-        <button onclick="setDifficulty(this)" data-difficulty="15" class="button-choix">15</button>
-        <button onclick="setDifficulty(this)" data-difficulty="25" class="button-choix">25</button>
-        <button onclick="setDifficulty(this)" data-difficulty="999" class="button-choix">999</button>
-    </div>
+        <div class="button-container">
+            <button onclick="setDifficulty(this)" data-difficulty="?">?</button>
+            <button onclick="setDifficulty(this)" data-difficulty="1">1</button>
+            <button onclick="setDifficulty(this)" data-difficulty="3">3</button>
+            <button onclick="setDifficulty(this)" data-difficulty="5">5</button>
+            <button onclick="setDifficulty(this)" data-difficulty="10">10</button>
+            <button onclick="setDifficulty(this)" data-difficulty="15">15</button>
+            <button onclick="setDifficulty(this)" data-difficulty="25">25</button>
+            <button onclick="setDifficulty(this)" data-difficulty="999">999</button>
+        </div>
 
-    <span id="char-count">Poster un commentaire (255 caractères restants)</span>
-    <br>
-    <form action="../process/pokerplanning_process.php" method="POST">
-        <input type="hidden" name="userId" value="<?php echo isset($_SESSION['IdUser']) ? htmlspecialchars($_SESSION['IdUser']) : ''; ?>">
-        <input type="hidden" name="IdT" value="<?php echo isset($currentTask["IdT"]) ? htmlspecialchars($currentTask["IdT"]) : '?'; ?>">
-        <textarea id="comment" name="commentContent" maxlength="255" oninput="adjustTextAreaSize(this), ajustRemainChar(this)"></textarea>
-        <input type="hidden" name="difficulty" value="<?php echo isset($difficulty) ? htmlspecialchars($difficulty) : '?'; ?>">
-        <input type="submit" class="next-button" value="Suivant">
-    </form>
+        <span id="char-count">Poster un commentaire (255 caractères restants)</span>
+        <br>
+        <form action="../process/pokerplanning_process.php" method="POST">
+            <input type="hidden" name="userId" value="<?php echo isset($_SESSION['IdUser']) ? htmlspecialchars($_SESSION['IdUser']) : ''; ?>">
+            <input type="hidden" name="IdT" value="<?php echo $taskId; ?>">
+            <textarea id="comment" name="commentContent" maxlength="255" oninput="adjustTextAreaSize(this), ajustRemainChar(this)"></textarea>
+            <input type="hidden" name="difficulty" value="<?php echo isset($difficulty) ? htmlspecialchars($difficulty) : '?'; ?>">
+            <input type="submit" class="next-button" value="Suivant">
+        </form>
 
-    <a href="./planning_poker_resume.php">Planning formulaire resume </a>
+        <a href="./planning_poker_resume.php">Planning formulaire resume </a>
+    <?php endif; ?>
 
     <script src="../scripts/scripts.js"></script>
 </body>
--- Site/process/pokerplanning_process.php
+++ Site/process/pokerplanning_process.php
@@ -1,7 +1,10 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 include "../process/function_tasks.php";
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_POST['userId'];
@@ -9,14 +12,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $commentContent = $_POST['commentContent'];
     $difficulty = $_POST['difficulty'];
 
-    //insertCout($userId, $idT, $commentContent, $difficulty);
+    $nextPage = $_SESSION['NextPage'] + 1;
+    $_SESSION['NextPage'] = $nextPage;
+    $_SESSION['SessionPage'] = $nextPage;
 
-    $nextPage = $idT +1;
-    
     $baseUrl = '../pages/';
     $redirectUrl = $baseUrl . 'planning_poker.php?page=' . $nextPage;
 
+    insertCout($userId, $idT, $commentContent, $difficulty);
+
     header("Location: " . $redirectUrl);
-    exit();
 }
-?>
