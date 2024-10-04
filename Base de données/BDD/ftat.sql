773e37f09ff9fb4bd413c35dd610392e5923c2a1
Jules G. <jules.grdn@gmail.com>
2024-10-04 12:19:45 +0200

    fix last bug

--- Base de données/BDD/ftat.sql
+++ Base de données/BDD/ftat.sql
@@ -462,4 +462,26 @@ END; $$
 
 DELIMITER ;
 
+DELIMITER $$
+
+CREATE FUNCTION getIdRole(role VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE roleId INT;
+
+    IF role IN ('Scrum Master', 'Product Owner', 'Member') THEN
+        SELECT IdR
+        INTO roleId
+        FROM ftat.roles
+        WHERE DescR = role;
+
+        RETURN roleId;
+    ELSE
+        RETURN -1;
+    END IF;
+END $$
+
+DELIMITER ;
+
 COMMIT;
