0264b254998faae6bdb80a02f72626b1acdec842
Malgrave <rodeurwill@gmail.com>
2024-10-04 11:10:37 +0200

    fix srum pp

--- Site/pages/planning_poker_scrum.php
+++ Site/pages/planning_poker_scrum.php
@@ -16,8 +16,6 @@
         include_once "../process/function_tasks.php";
         $results = displayAllVotes($_SESSION["currPrj"]);
         $difficulties = displayAllDifficulties();
-        $taskTitles = array();
-
         if ($results && count($results) > 0):
         ?>
             <table border="1">
@@ -32,7 +30,6 @@
                     <th>25</th>
                     <th>999</th>
                     <th>Choix le plus voté</th>
-
                 </tr>
                 <?php
                 foreach ($results as $row):
@@ -41,16 +38,36 @@
                         <?php foreach ($row as $key => $value): ?>
                             <td><?php echo isset($value) ? $value : ''; ?></td>
                         <?php endforeach; ?>
+                        <td><?php echo getMostVotedChoice($row); ?></td>
                     </tr>
-                <?php
-                endforeach;
-                ?>
+                <?php endforeach; ?>
             </table>
         <?php else: echo "<p class='no-results'>Aucune tâche trouvée.</p>";
-        endif; ?>
+        endif;
+        ?>
+
         <a href="./planning_poker.php" class="btn">Planning formulaire</a>
     </main>
     <script src="../scripts/scripts.js"></script>
 </body>
 
 </html>
+<?php
+function getMostVotedChoice($row)
+{
+    $choices = ['?', '1', '3', '5', '10', '15', '25', '999'];
+    $votes = [
+        '?'   => $row['Occurrence_'],
+        '1'   => $row['Occurrence_1'],
+        '3'   => $row['Occurrence_3'],
+        '5'   => $row['Occurrence_5'],
+        '10'  => $row['Occurrence_10'],
+        '15'  => $row['Occurrence_15'],
+        '25'  => $row['Occurrence_25'],
+        '999' => $row['Occurrence_999']
+    ];
+    $maxVotes = max($votes);
+    $mostVotedChoices = array_keys($votes, $maxVotes);
+    return implode(', ', $mostVotedChoices);
+}
+?>
